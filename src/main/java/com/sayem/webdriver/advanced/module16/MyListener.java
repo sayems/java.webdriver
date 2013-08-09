45974d39d845dae76016caef88e24bfa9dded93e
Syed Sayem <syed@hotmail.com>
2013-08-09 12:48:14 -0400

    Cleaning up

---
+++ src/main/java/com/sayem/webdriver/advanced/module16/MyListener.java
@@ -0,0 +1,14 @@
+package com.sayem.webdriver.advanced.module16;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+
+
+public class MyListener extends AbstractWebDriverEventListener{
+	
+	
+	public void afterNavigateBack(WebDriver driver) {
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/main/java/com/sayem/webdriver/basic/App.java
@@ -0,0 +1,7 @@
+package com.sayem.webdriver.basic;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to WebDriver Basic");
+    }
+}
