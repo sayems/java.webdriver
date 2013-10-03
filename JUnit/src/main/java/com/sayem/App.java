fdf2ac24bb8a003a1abb2362844bff7a575e0a89
Syed Sayem <syed@hotmail.com>
2013-10-03 11:16:44 -0400

    Added JUnit module

---
+++ JUnit/src/main/java/com/sayem/App.java
@@ -0,0 +1,13 @@
+package com.sayem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
