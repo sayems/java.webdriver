68834e4fa75436a12cc76416909b49a622c94d69
Syed Sayem <syed@hotmail.com>
2013-10-03 08:20:37 -0400

    Added FirefoxBrowser examples

---
+++ Examples/src/main/java/com/sayem/fluentwait/FluentDriver.java
@@ -0,0 +1,11 @@
+package com.sayem.fluentwait;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 12/24/12
+ * Time: 7:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FluentDriver {
+}
