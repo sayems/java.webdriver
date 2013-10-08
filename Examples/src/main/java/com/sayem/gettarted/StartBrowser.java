202037a195d2e64bbdedfc9de52bf5438a2d6fd9
Syed Sayem <syed@hotmail.com>
2013-10-07 22:26:08 -0400

    Added StartBrowser example

---
+++ Examples/src/main/java/com/sayem/gettarted/StartBrowser.java
@@ -0,0 +1,13 @@
+package com.sayem.gettarted;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class StartBrowser {
+
+	public static void main(String[] args) {
+		WebDriver dr = new FirefoxDriver();
+	}
+
+}
