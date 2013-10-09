8d9f9cfe924f74b4bce49fbea04c092741fceb23
Syed Sayem <syed@hotmail.com>
2013-10-09 08:31:40 -0400

    Added Attribute class & attribute.html & input.html file

---
+++ Examples/src/main/java/com/sayem/browser/attribute/Attribute.java
@@ -0,0 +1,35 @@
+package com.sayem.browser.attribute;
+
+import java.io.File;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Attribute {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver dr = new ChromeDriver();
+		
+		File file = new File("src/attribute.html");
+		String filePath = "file:///" + file.getAbsolutePath();
+		System.out.printf("now accesss %s \n", filePath);
+		
+		dr.get(filePath);
+		Thread.sleep(1000);
+		
+		WebElement link = dr.findElement(By.id("tooltip"));
+		
+		System.out.println(link.getAttribute("data-original-title"));
+		
+		System.out.println(link.getText());
+		
+		Thread.sleep(1000);
+		System.out.println("browser will be close");
+		dr.quit();	
+	}
+
+}
