df701c84ebb37e88eb93831e13a7ad5135aef474
Syed Sayem <syed@hotmail.com>
2013-10-19 09:58:33 -0400

    Added FindByXpathExampleTest

---
+++ src/test/java/com/sayem/basics/interrogate/findby/FindByXpathExampleTest.java
@@ -0,0 +1,83 @@
+package com.sayem.basics.interrogate.findby;
+
+import com.sayem.webdriver.examples.Driver;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindByXpathExampleTest {
+
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void createDriverAndVisitTestPage(){
+        //driver = new FirefoxDriver();
+        driver = Driver.get("http://www.compendiumdev.co.uk" +
+                "/selenium/find_by_playground.php");
+    }
+
+    @Test
+    public void assertNumberOfParagraphs(){
+
+        List<WebElement> elements;
+        elements = driver.findElements(By.xpath("//p"));
+
+        int nestedCount = 0;
+        for(WebElement e : elements){
+            if(e.getText().contains("nested para")){
+                nestedCount++;
+            }
+        }
+        assertEquals(16,nestedCount);
+        assertEquals(41, elements.size());
+    }
+
+    @Test
+    public void findSpecificPara(){
+
+        WebElement element;
+        element = driver.findElement(
+                By.xpath("//p[@name='pName5']"));
+
+        assertEquals("Expected matching id",
+                "p5",
+                element.getAttribute("id"));
+    }
+
+    @Test
+    public void pathNavigation(){
+
+        WebElement element;
+        element = driver.findElement(
+                By.xpath("//div[@id='div18']//a[@name='aName26']"));
+
+        assertEquals("Expected matching id",
+                "a26",
+                element.getAttribute("id"));
+    }
+
+    @Test
+    public void conditionalAttributes(){
+
+        WebElement element;
+        element = driver.findElement(
+                By.xpath("//a[@name='aName26' and @class='normal']"));
+
+        assertEquals("Expected matching id",
+                     "a26",
+                     element.getAttribute("id"));
+    }
+
+
+    @AfterClass
+    public static void closeBrowser(){
+        //driver.quit();
+    }
+}
