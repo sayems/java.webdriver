44160af84de0628fc34933d709457b7f5a0ceddb
Syed Sayem <syed@hotmail.com>
2013-10-22 07:40:09 -0400

    Added OperaDriverTest

---
+++ src/test/java/com/sayem/drivers/OperaDriverTest.java
@@ -0,0 +1,67 @@
+package com.sayem.drivers;
+
+import com.opera.core.systems.OperaDriver;
+import com.opera.core.systems.OperaProfile;
+import com.opera.core.systems.preferences.OperaPreferences;
+import com.sayem.webdriver.examples.Driver;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class OperaDriverTest {
+
+    @Test
+    public void basicOperaDriverUsage(){
+        WebDriver opera = new OperaDriver();
+
+        opera.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
+
+        assertThat(opera.getTitle(), is("HTML Form Elements"));
+
+        opera.quit();
+    }
+
+    @Test
+    public void capabilitiesOperaDriverUsage(){
+
+        org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
+        proxy.setHttpProxy(Driver.PROXY)
+                .setFtpProxy(Driver.PROXY)
+                .setSslProxy(Driver.PROXY);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.PROXY, proxy);
+
+        WebDriver opera = new OperaDriver(capabilities);
+
+        opera.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
+
+        assertThat(opera.getTitle(), is("HTML Form Elements"));
+
+        opera.quit();
+    }
+
+    // opera also has preferences that can be used
+    //opera:config
+    //http://www.opera.com/support/usingopera/operaini
+    @Test
+    public void preferencesOperaDriverUsage(){
+
+
+        OperaProfile profile = new OperaProfile();
+        // switching off Javascript will cause the opera driver to fail
+        profile.preferences().set("Extensions", "Scripting", 0);
+
+        WebDriver opera = new OperaDriver(profile);
+
+        opera.get("http://www.compendiumdev.co.uk/selenium/basic_html_form.html");
+
+        assertThat(opera.getTitle(), is("HTML Form Elements"));
+
+        opera.quit();
+    }
+
+}
