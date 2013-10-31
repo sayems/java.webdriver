4a23c7c90140b8d18382ea0af0438a4d18e4f01e
Syed Sayem <syed@hotmail.com>
2013-10-31 16:54:26 -0400

    Added ElementTests

---
+++ src/main/java/com/sayem/webdriver/examples/ElementTests.java
@@ -0,0 +1,63 @@
+package com.sayem.webdriver.examples;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class ElementTests {
+	
+	static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp()
+	{
+		driver = new ChromeDriver();
+		driver.get("http://dl.dropbox.com/u/55228056/DoubleClickDemo.html");
+	}
+	
+	@Test
+	public void testElementText()
+	{
+		//Get the message Element
+		WebElement message = driver.findElement(By.id("message"));
+		
+		//Get the message elements text
+		String messageText = message.getText();
+		
+		//Verify message element's text displays "Click on me and my color will change"
+		assertEquals("Click on me and my color will change", messageText);
+		
+		//Get the area Element
+		WebElement area = driver.findElement(By.id("area"));
+		
+		//Verify area element's text displays "Div's Text\nSpan's Text"
+		assertEquals("Div's Text\nSpan's Text",area.getText());
+	}
+	
+	@Test
+	public void testElementAttribute()
+	{
+		WebElement message = driver.findElement(By.id("message"));
+		assertEquals("justify",message.getAttribute("align"));
+	}
+	
+	@Test
+	public void testElementStyle()
+	{
+		WebElement message = driver.findElement(By.id("message"));
+		String width = message.getCssValue("width");
+		assertEquals("150px",width);
+	}
+	
+	@AfterClass
+	public static void tearDown()
+	{
+		driver.close();
+	}
+}
