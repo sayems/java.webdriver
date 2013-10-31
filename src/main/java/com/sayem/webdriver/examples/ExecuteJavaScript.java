9dfce5c34963e7cbdc40f5e294a699a4fa273895
Syed Sayem <syed@hotmail.com>
2013-10-31 16:54:41 -0400

    Added ExecuteJavaScript

---
+++ src/main/java/com/sayem/webdriver/examples/ExecuteJavaScript.java
@@ -0,0 +1,28 @@
+package com.sayem.webdriver.examples;
+
+import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExecuteJavaScript {
+	@Test
+	public void testJavaScriptCalls() throws Exception
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+			
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		String title = (String) js.executeScript("return document.title");
+		assertEquals("Google", title);
+		
+		long links = (Long) js.executeScript("var links = document.getElementsByTagName('A'); return links.length");
+		assertEquals(42, links);
+			
+		driver.close();
+			
+	}
+}
