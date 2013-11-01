462643dad5f5e341613b6f5522e8e81f6236f36b
Syed Sayem <syed@hotmail.com>
2013-11-01 10:21:18 -0400

    Added testBmiCalculation

---
+++ PageObject/src/test/java/com/sayem/test/BmiCalculatorTests.java
@@ -0,0 +1,30 @@
+package com.sayem.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import seleniumcookbook.tests.pageobjects.*;
+
+public class BmiCalculatorTests {
+
+	@Test
+	public void testBmiCalculation()
+	{
+		//Create an instance of Bmi Calculator Page class
+		//and provide the driver
+		BmiCalcPage bmiCalcPage = new BmiCalcPage();
+		
+		//Open the Bmi Calculator Page
+		bmiCalcPage.get();
+
+		//Calculate the Bmi by supplying Height and Weight values
+		bmiCalcPage.calculateBmi("181", "80");
+
+		//Verify Bmi & Bmi Category values
+		assertEquals("24.4", bmiCalcPage.getBmi());
+		assertEquals("Normal", bmiCalcPage.getBmiCategory());
+		
+		//Close the Bmi Calculator Page
+		bmiCalcPage.close();
+	}
+}
--- src/main/java/com/sayem/webdriver/examples/ImplicitWaitTest.java
+++ src/main/java/com/sayem/webdriver/examples/ImplicitWaitTest.java
@@ -13,6 +13,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class ImplicitWaitTest {
+
  	@Test
  	public void testWithOutWait()
  	{
