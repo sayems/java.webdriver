e44d17d752bef8427fecd99528e2619d34ba4831
Syed Sayem <syed@hotmail.com>
2013-11-02 10:22:37 -0400

    Added WebTableTests

---
+++ src/main/java/com/sayem/webdriver/examples/webtable/WebTableTests.java
@@ -0,0 +1,62 @@
+package com.sayem.webdriver.examples.webtable;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WebTableTests {
+	
+	private WebDriver driver;
+	private StringBuffer verificationErrors = new StringBuffer();
+	
+	@Before
+	public void setUp() {
+		// Create a new instance of the Firefox driver
+		driver = new FirefoxDriver();
+		driver.get("http://dl.dropbox.com/u/55228056/Locators.html");
+	}
+
+	@Test
+	public void testWebTableTests() {
+		try {
+			
+			//Get the table element as WebTable instance using CSS Selector
+			WebTable table = new WebTable(driver.findElement(By.cssSelector("div.cart-info table")));
+	
+			//Verify that it has three rows
+			assertEquals(3,table.getRowCount());
+			//Verify that it has six columns
+			assertEquals(5,table.getColumnCount());
+			//Verify that specified value exists in second cell of third row
+			assertEquals("iPhone",table.getCellData(3,1));
+			
+			//Get in cell editor and enter some value 
+			WebElement cellEdit = table.getCellEditor(3,3,0);
+			cellEdit.clear();
+			cellEdit.sendKeys("2");
+			
+		} catch (Error e) {
+			//Capture and append Exceptions/Errors
+			verificationErrors.append(e.toString());
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		
+		//Close the browser
+		driver.quit();
+		
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+}
