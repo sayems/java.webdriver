b7c50fd8568113db4cabb18b61e9a562d3dc3e7b
Syed Sayem <syed@hotmail.com>
2013-11-25 18:57:35 -0500

    Added DesiredCapabilitiesExample

--- src/main/java/com/sayem/webdriver/basic/App.java
+++
@@ -1,7 +0,0 @@
-package com.sayem.webdriver.basic;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Welcome to WebDriver Basic");
-    }
-}
---
+++ src/main/java/com/sayem/webdriver/basic/DesiredCapabilitiesExample.java
@@ -0,0 +1,26 @@
+package com.sayem.webdriver.basic;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class DesiredCapabilitiesExample {
+
+
+    public static void main(String[] args) {
+
+        DesiredCapabilities capailities = new DesiredCapabilities();
+        capailities.setJavascriptEnabled(true);
+        //capailities.setCapability(CapabilityType.PROXY, value);
+
+
+        FirefoxDriver driver = new FirefoxDriver(capailities);
+
+        System.setProperty("webdriver.chrome.driver", "H:\\chromedriver.exe");
+        ChromeDriver d1 = new ChromeDriver(capailities);
+
+
+
+    }
+
+}
