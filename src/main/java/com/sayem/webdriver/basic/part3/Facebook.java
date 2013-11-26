0e9c07c00114e606b46fce42ad972635dfe5533a
Syed Sayem <syed@hotmail.com>
2013-11-26 09:12:01 -0500

    Added Facebook

---
+++ src/main/java/com/sayem/webdriver/basic/part3/Facebook.java
@@ -0,0 +1,28 @@
+package com.sayem.webdriver.basic.part3;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Facebook {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://facebook.com");
+		List<WebElement> fields = driver.findElements(By.xpath("//input[@type='text' or @type='password']"));
+		System.out.println(fields.size());
+		
+		fields.get(0).sendKeys("one");
+		fields.get(1).sendKeys("two");
+		fields.get(2).sendKeys("three");
+		fields.get(4).sendKeys("four");
+
+		
+	}
+
+}
