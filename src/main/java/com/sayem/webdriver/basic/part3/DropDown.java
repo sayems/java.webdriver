df49b5fa0080e3530451d35988ea7bf74a1c6c49
Syed Sayem <syed@hotmail.com>
2013-11-26 09:10:24 -0500

    Added DropDown

---
+++ src/main/java/com/sayem/webdriver/basic/part3/DropDown.java
@@ -0,0 +1,38 @@
+package com.sayem.webdriver.basic.part3;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DropDown {
+
+    public static void main(String[] args) {
+
+        // select , find total , print ,selected
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://timesofindia.com");
+        WebElement droplist = driver.findElement(By.xpath("//select[@id='cityselection']"));
+        droplist.sendKeys("Goa");
+        List<WebElement> allOptions = droplist.findElements(By.tagName("option"));
+        System.out.println("Total options in list -> "+ allOptions.size());
+
+
+
+        for(int i=0;i<allOptions.size();i++){
+            System.out.println(allOptions.get(i).getText()+" ---- "+ allOptions.get(i).getAttribute("selected"));
+        }
+
+        // select by index - 5th
+
+        // 2nd way
+        Select s = new Select(droplist);
+        // YOU
+        /// www.hdfcbank.com
+
+    }
+
+}
