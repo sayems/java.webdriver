e38758c37c5ba22c8ce87e4252e0b68fba570931
Syed Sayem <syed@hotmail.com>
2013-11-25 19:03:50 -0500

    Added SeleniumBasics

---
+++ src/main/java/com/sayem/webdriver/basic/part1/SeleniumBasics.java
@@ -0,0 +1,34 @@
+package com.sayem.webdriver.basic.part1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class SeleniumBasics {
+
+
+    public static void main(String[] args) {
+        // cntrl + shift  + o
+        // 18
+
+        // find the binary
+        //FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox 4.0 Beta 8\\firefox.exe"));
+        //FirefoxDriver driver1 = new FirefoxDriver(binary, null);
+
+
+        FirefoxDriver driver = new FirefoxDriver();
+
+        // Chrome
+        //System.setProperty("webdriver.chrome.driver", "H:\\chromedriver.exe");
+        //ChromeDriver d1 = new ChromeDriver();
+
+        // IE - protected mode as at same level
+        // download exe
+        //System.setProperty("webdriver.ie.driver", "H:\\IEDriverServer.exe");
+        //InternetExplorerDriver ie = new InternetExplorerDriver();
+
+
+    }
+
+}
