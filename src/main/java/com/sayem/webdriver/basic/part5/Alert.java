61124e021200d53281ecc90b25f72fc0d0834e4c
Syed Sayem <syed@hotmail.com>
2013-11-30 09:38:23 -0500

    Added Alert

---
+++ src/main/java/com/sayem/webdriver/basic/part5/Alert.java
@@ -0,0 +1,24 @@
+package com.sayem.webdriver.basic.part5;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Alert {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://in.rediff.com");
+        driver.findElement(By.linkText("Sign in")).click();
+        driver.findElement(By.xpath("//*[@id='btn_login']")).click();
+        Thread.sleep(5000);
+        org.openqa.selenium.Alert al = driver.switchTo().alert();
+        System.out.println(al.getText());
+        al.accept();//OK
+        //al.dismiss();//cancel
+        // switch to regular page
+        driver.switchTo().defaultContent();
+        // normal commands
+    }
+
+}
