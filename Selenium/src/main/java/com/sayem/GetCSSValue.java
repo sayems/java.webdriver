31937016a57547c7d5f9a3e50589eaa4de649d1c
Syed Sayem <syed@hotmail.com>
2014-02-02 14:41:50 -0500

    How to getCSSValue in WebDriver

---
+++ Selenium/src/main/java/com/sayem/GetCSSValue.java
@@ -0,0 +1,15 @@
+package com.sayem;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GetCSSValue {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        System.out.println(searchButton.getCssValue("font-family"));
+    }
+}
