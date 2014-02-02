8538029fc95c7415a2aaf24d2445e1d0733fd530
Syed Sayem <syed@hotmail.com>
2014-02-02 14:39:41 -0500

    How to clear in WebDriver

---
+++ Selenium/src/main/java/com/sayem/Clear.java
@@ -0,0 +1,18 @@
+package com.sayem;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Clear {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys(Keys.chord(Keys.SHIFT, "packt publishing"));
+        searchBox.clear();
+    }
+}
