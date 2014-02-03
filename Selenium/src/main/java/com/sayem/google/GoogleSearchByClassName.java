bebbf0bcaadfcb9750a6be2affdeb84a96814191
Syed Sayem <syed@hotmail.com>
2014-02-03 14:37:28 -0500

    How to GoogleSearchByClassName

---
+++ Selenium/src/main/java/com/sayem/google/GoogleSearchByClassName.java
@@ -0,0 +1,15 @@
+package com.sayem.google;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleSearchByClassName {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://www.google.com");
+        WebElement searchBox = driver.findElement(By.className("gbqfif"));
+        searchBox.sendKeys("Packt Publishing");
+    }
+}
