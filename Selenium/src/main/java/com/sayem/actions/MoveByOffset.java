ab81f1ba76b5a7684c2e9c4032335d5ef38e5c26
Syed Sayem <syed@hotmail.com>
2014-02-06 19:55:52 -0500

    Added MoveByOffSet

--- Selenium/src/main/java/com/sayem/actions/MoveByOffset.java
+++ Selenium/src/main/java/com/sayem/actions/MoveByOffset.java
@@ -1,4 +1,21 @@
 package com.sayem.actions;
 
-public class MoveByOffset {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class MoveByOffSet {
+    public static void main(String... args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("file://C:/Selectable.html");
+        WebElement three = driver.findElement(By.name("three"));
+        System.out.println("X coordinate: " + three.getLocation().getX()
+                + "  Y coordinate: " + three.getLocation().getY());
+        Actions builder = new Actions(driver);
+        builder.moveByOffset(three.getLocation().getX() + 1, three
+                .getLocation().getY() + 1);
+        builder.perform();
+    }
 }
